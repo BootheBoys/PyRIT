bc6d8b719d8a12c3bb560712e8b1ef683b2d228d
BootheBoys <32530293+BootheBoys@users.noreply.github.com>
2024-06-20 19:11:15 -0500

    Update run_new.py

--- run_new.py
+++ run_new.py
@@ -31,6 +31,9 @@ def generate_response(model, tokenizer, prompt, preamble=None):
     print(outputs[0])
     print("------------------------------")
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    print("OUTPUTS after TOKEN: ---------------------")
+    print(outputs[0])
+    print("------------------------------")
     return response
 
 # Initialize conversation log
